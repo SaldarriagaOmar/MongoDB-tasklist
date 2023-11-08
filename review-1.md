b52f3e05190f3524a8ad249d336dbb71c19ccefc
Ómar Saldarriaga <saldarriagaomar@gmail.com>
2023-11-07 23:37:12 -0500

    review commit actualizado

--- review-1.md
+++ review-1.md
@@ -1,17 +1,17 @@
 Acerca de la App
 
-    Nuestra aplicación es como una especie de libreta en la que puedes anotar todas las cosas que necesitas hacer. La puedes usar para llevar un registro de tus actividades diarias, tus proyectos o cualquier tarea que tengas en mente. Puedes agregar nuevas tareas, borrar las que ya has completado y ver la lista de tareas en cualquier momento.
+    Nuestra aplicación de gestión de tareas es una herramienta versátil que te permite llevar un registro de tus actividades diarias, proyectos pendientes y cualquier tarea que tengas en mente. A través de una interfaz de línea de comandos, puedes agregar nuevas tareas, eliminar las que ya has completado y ver la lista de tareas en cualquier momento. ¡Simplifica tu organización y mantén un control efectivo de tus pendientes!
 
 Funcionalidades Importantes
 
-    Las partes más útiles de la aplicación son:
+Las partes más destacadas de la aplicación incluyen:
 
-    Agregar tareas: Cuando quieras apuntar una tarea, solo necesitas seleccionar la opción "Agregar" y escribir una breve descripción de lo que tienes que hacer.
+1. Agregar Tareas: Cuando necesitas registrar una nueva tarea, selecciona la opción "Agregar", escribe una breve descripción y la tarea quedará registrada en tu lista de pendientes.
 
-    Eliminar tareas: Si decides que una tarea ya no es relevante, puedes eliminarla seleccionando la opción "Eliminar" y escribiendo el número de esa tarea.
+2. Eliminar Tareas: Si consideras que una tarea ya no es relevante, puedes eliminarla fácilmente. Simplemente selecciona la opción "Eliminar" y escribe el número de la tarea que deseas quitar de tu lista.
 
-    Marcar como completada: Cuando termines una tarea, simplemente selecciona la opción "Completar" y escribe el número de la tarea que has finalizado.
+3. Marcar como Completada: Cuando hayas terminado una tarea, selecciona la opción "Completar" y especifica el número de la tarea completada. La aplicación la marcará como finalizada y actualizará la lista.
 
-    Ver la lista completa: En cualquier momento, puedes ver la lista completa de tareas eligiendo la opción "Mostrar", y la lista se actualizará al instante.
+4. Ver Lista Completa: En cualquier momento, puedes consultar la lista completa de tus tareas pendientes seleccionando la opción "Mostrar". La lista se actualiza al instante para que tengas una vista clara de tus pendientes.
 
-Y si en algún momento decides que ya no necesitas la lista de tareas, puedes salir de la aplicación con la opción "Salir". ¡Así de fácil!
+Si en algún momento decides que ya no necesitas tu lista de tareas, puedes salir de la aplicación con la opción "Salir". ¡Sencillez y eficacia para mantener tu organización en un solo lugar!
--- script.js
+++ script.js
@@ -1,119 +1,80 @@
-const { MongoClient } = require('mongodb');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost:27017/task-list';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const connectionString = 'mongodb://localhost:27017';
-const dbName = 'task-list';
-const collectionName = 'tareas';
+const db = mongoose.connection;
 
-const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión a MongoDB establecida.');
+});
 
-let db;
-let tasksCollection;
+const mongoose = require('mongoose');
 
-function connectToDatabase() {
-  return new Promise(async (resolve, reject) => {
-    try {
-      await client.connect();
-      db = client.db(dbName);
-      tasksCollection = db.collection(collectionName);
-      console.log('Conexión a la base de datos establecida.');
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
+const taskSchema = new mongoose.Schema({
+  description: String,
+  completed: Boolean,
+});
+
+const Task = mongoose.model('Task', taskSchema);
 
 async function addTask(description) {
-  const task = {
-    description,
-    completed: false,
-  };
-  await tasksCollection.insertOne(task);
-  console.log('Tarea agregada con éxito.');
-}
-
-async function removeTask(index) {
-  const result = await tasksCollection.deleteOne({ _id: tasksCollection[index]._id });
-  if (result.deletedCount === 1) {
-    console.log('Tarea eliminada con éxito.');
-  } else {
-    console.log('Índice incorrecto o tarea no encontrada.');
+    const task = new Task({
+      description,
+      completed: false,
+    });
+  
+    try {
+      await task.save();
+      console.log('Tarea agregada con éxito.');
+    } catch (error) {
+      console.error('Error al agregar tarea:', error);
+    }
   }
-}
-
-async function completeTask(index) {
-  const result = await tasksCollection.updateOne(
-    { _id: tasksCollection[index]._id },
-    { $set: { completed: true } }
-  );
-  if (result.modifiedCount === 1) {
-    console.log('Tarea marcada como completada.');
-  } else {
-    console.log('Índice incorrecto o tarea no encontrada.');
-  }
-}
-
-async function showTasks() {
-  const tasks = await tasksCollection.find().toArray();
-  console.log('Lista de tareas:');
-  tasks.forEach((task, index) => {
-    console.log(`${index}. [${task.completed ? 'x' : ' '}] ${task.description}`);
-  });
-}
-
-rl.setPrompt('Comando (add, remove, complete, show, exit): ');
-rl.prompt();
-
-(async () => {
-  try {
-    await connectToDatabase();
-
-    rl.on('line', async (line) => {
-      const command = line.trim().toLowerCase();
-
-      switch (command) {
-        case 'add':
-          rl.question('Tarea: ', async (description) => {
-            await addTask(description);
-            rl.prompt();
-          });
-          break;
-        case 'remove':
-          rl.question('Índice de la tarea que desea eliminar: ', async (index) => {
-            await removeTask(parseInt(index));
-            rl.prompt();
-          });
-          break;
-        case 'complete':
-          rl.question('Índice de la tarea completada: ', async (index) => {
-            await completeTask(parseInt(index));
-            rl.prompt();
-          });
-          break;
-        case 'show':
-          await showTasks();
-          rl.prompt();
-          break;
-        case 'exit':
-          rl.close();
-          break;
-        default:
-          console.log('Comando incorrecto.');
-          rl.prompt();
-          break;
+  
+  async function removeTask(index) {
+    try {
+      const result = await Task.deleteOne({ _id: index });
+      if (result.deletedCount === 1) {
+        console.log('Tarea eliminada con éxito.');
+      } else {
+        console.log('Índice incorrecto o tarea no encontrada.');
       }
-    });
-
-    rl.on('close', () => {
-      console.log('¡Hasta luego!');
-      process.exit(0);
-    });
-  } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+    } catch (error) {
+      console.error('Error al eliminar tarea:', error);
+    }
   }
-})();
+  
+  async function completeTask(index) {
+    try {
+      const result = await Task.updateOne(
+        { _id: index },
+        { $set: { completed: true } }
+      );
+      if (result.modifiedCount === 1) {
+        console.log('Tarea marcada como completada.');
+      } else {
+        console.log('Índice incorrecto o tarea no encontrada.');
+      }
+    } catch (error) {
+      console.error('Error al marcar tarea como completada:', error);
+    }
+  }
+  
+  async function showTasks() {
+    try {
+      const tasks = await Task.find().exec();
+      console.log('Lista de tareas:');
+      tasks.forEach((task, index) => {
+        console.log(`${index}. [${task.completed ? 'x' : ' '}] ${task.description}`);
+      });
+    } catch (error) {
+      console.error('Error al mostrar tareas:', error);
+    }
+  }
+  
